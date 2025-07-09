8bb77fe69d317516f342a06f680affd9243dd77e
KimJeeHoon <rwrwg159@gmail.com>
2025-07-09 16:10:44 +0900

    몬스터가 약간 밑에서 생성되던 버그 수정

--- Assets/02_Scripts/Develop_Team_1_Scripts/Jihoon/Mon005Generator.cs
+++ Assets/02_Scripts/Develop_Team_1_Scripts/Jihoon/Mon005Generator.cs
@@ -28,13 +28,14 @@ public class Mon005Generator : MonsterGenerator
         float radius = (rand > 0.995f) ? genRadius : 0.4f * genRadius * Mathf.Sqrt(-Mathf.Log(1 - rand)) + 1.5f;
         float angle = 2f * Mathf.PI * Random.Range(0f, 1f);
 
-        Vector3 pos = new Vector3(radius * Mathf.Cos(angle), 1f, radius * Mathf.Sin(angle));
+        Vector3 pos = new Vector3(radius * Mathf.Cos(angle), 0, radius * Mathf.Sin(angle));
         pos += playerTransform.position;
         if (PlaySystemRefStorage.mapSetter.IsInMap(pos) == false)
         {
             pos = PlaySystemRefStorage.mapSetter.FindNearestPoint(pos);
         }
 
+        pos.y = 1f;
         return pos;
     }
 
